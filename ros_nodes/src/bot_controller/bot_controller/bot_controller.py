043e286349b54bab82b31b40c2eb43aeb2204dff
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2025-01-25 01:45:54 +0600

    Refactor bot_controller to improve data reading and publish commands to turtle

--- ros_nodes/src/bot_controller/bot_controller/bot_controller.py
+++ ros_nodes/src/bot_controller/bot_controller/bot_controller.py
@@ -15,26 +15,28 @@ class cleaning_bot(Node):
         self.ser.write(f'a'.encode())   
         self.get_logger().info("Connected with arduino")
 
-        self.create_timer(0.0000000000000001,self.read_data)
-
-        #new twist message
-        twist = Twist.Twist()
-        twist.linear.x = 0.5
-        twist.angular.z = 0.5
+        self.create_timer(0.0000001,self.read_data)
+        self.turtle_publisher = self.create_publisher(Twist.Twist, '/turtle1/cmd_vel', 10)
         self.subscription = self.create_subscription(Twist.Twist, 'acb_cmd_vel', self.send_motor_data_to_arduino, 10)
 
 
     def send_motor_data_to_arduino(self,msg):
         throttle = msg.linear.x
         steering = msg.angular.z
-        self.get_logger().info(f"throttle: {throttle} steering: {steering}")
-        # command = f"L{throttle:.2f} A{steering:.2f} B{15} R{16.5} M{7.2}\n"
-        # self.ser.write(command.encode())
+        new_twist = Twist.Twist()
+        new_twist.linear.x = throttle/100.0
+        new_twist.angular.z = steering/100.0
+        self.turtle_publisher.publish(new_twist)
+        command = f"L{throttle:.2f} A{steering:.2f} B{15} R{16.5} M{7.2}\n"
+        self.ser.write(command.encode())
 
     def read_data(self):
-        data=self.ser.readline().decode()
-        if data:
-            self.get_logger().info(data)
+        try:
+            data=self.ser.readline().decode()
+            if data!="":
+                self.get_logger().info(data)
+        except:
+            self.get_logger().error("Could not read data")
         
 
 def main(args=None):
